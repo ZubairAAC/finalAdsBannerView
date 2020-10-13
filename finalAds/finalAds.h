b205b4c0d17973cf997c0c7f3f7af857c548ce37
zubair ayaz asim <zubairayazasim@ZAAC.local>
2020-10-13 16:36:17 +0500

    Initial Commit

---
+++ finalAds/finalAds.h
@@ -0,0 +1,19 @@
+//
+//  finalAds.h
+//  finalAds
+//
+//  Created by zubair ayaz asim on 13/10/2020.
+//  Copyright © 2020 zubair ayaz asim. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for finalAds.
+FOUNDATION_EXPORT double finalAdsVersionNumber;
+
+//! Project version string for finalAds.
+FOUNDATION_EXPORT const unsigned char finalAdsVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <finalAds/PublicHeader.h>
+
+
